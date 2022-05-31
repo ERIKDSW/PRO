da826f4d0dd65142d1edf9403cbc7b65e6e6f189
ERIKDSW <105739861+ERIKDSW@users.noreply.github.com>
2022-05-30 23:38:11 -0500

    Add files via upload

---
+++ TALLERPOO-master/geografia/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+      question: "¿En qué lugar hay actualmente pinturas del mesolítico?",
+      choices: ["España", "Mexico", "Argentina", "Alemania"],
+      answer: "España",
+    },
+    {
+      question: "historiaaaaaaaaaje no es orientado a Objetos",
+      choices: ["Java", "Haskell", "C++", "Python"],
+      answer: "Haskell",
+    },
+    {
+      question: "historiaaaaaaaaao la monalisa?",
+      choices: ["Leonardo Da Vinci", "Picasso", "Monet", "Vicent Van Goh", 'Frida Khalo', 'Diego Rivera'],
+      answer: "Leonardo Da Vinci",
+    },
+    {
+      question: "historiaaaaaaaaao la monalisa?",
+      choices: ["Leonardo Da Vinci", "Picasso", "Monet", "Vicent Van Goh", 'Frida Khalo', 'Diego Rivera'],
+      answer: "Leonardo Da Vinci",
+    },
+  ];
---
+++ TALLERPOO-master/programacion/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+      question: "programaciiiiiiiion siguientes opciones no es un editor de codigo?",
+      choices: ["vim", "vscode", "emacs", "word"],
+      answer: "word",
+    },
+    {
+      question: "brrrrrrrrrrrrrrrrrrr",
+      choices: ["Java", "Haskell", "C++", "Python"],
+      answer: "Haskell",
+    },
+    {
+      question: "gof?",
+      choices: ["Leonardo Da Vinci", "Picasso", "Monet", "Vicent Van Goh", 'Frida Khalo', 'Diego Rivera'],
+      answer: "Leonardo Da Vinci",
+    },
+    {
+      question: "yyyyyyyy?",
+      choices: ["Leonardo Da Vinci", "Picasso", "Monet", "Vicent Van Goh", 'Frida Khalo', 'Diego Rivera'],
+      answer: "Leonardo Da Vinci",
+    },
+  ];
